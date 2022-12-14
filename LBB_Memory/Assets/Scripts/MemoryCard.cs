07e060ce74f21857d4aebda28c71bd7e210381b8
DeltaTerran <Banelingrevolution@gmail.com>
2022-12-14 23:42:25 +0200

    5.2
    Создание карт и превращение их в интерактивные объекты

---
+++ LBB_Memory/Assets/Scripts/MemoryCard.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryCard : MonoBehaviour {
+	//Эта функция вызывается после щелчка на объекте.
+	[SerializeField] private GameObject cardBack;
+	public void OnMouseDown()
+    {
+		//Kод деактивации запускается только в случае, когда объект активен / видим.
+		if (cardBack.activeSelf)
+        {
+			cardBack.SetActive(false);
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
